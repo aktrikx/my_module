dd153a74f350c35d7c81ec805787000b2db513fb
Aktrikx <52633349+aktrikx@users.noreply.github.com>
2020-05-16 20:50:19 +0545

    Create mero_module.py

---
+++ mero_module.py
@@ -0,0 +1,10 @@
+def my_percent(a,b):
+  print((a/b)*100,"%")
+def my_add(a,b):
+    print(a+b)
+def my_sub(a,b):
+    print(a-b) 
+def my_divide(a,b):
+    print(a/b)   
+def my_multiply(a,b):
+    print(a*b)
